ca8cb5ee9b7338581fdab539d3a0a452112c6f5b
CENOS92 <beom5037@gmail.com>
2016-10-29 14:23:57 +0900

    유저 딕셔너리 변경/공유

--- compile-dictionary.sh
+++ compile-dictionary.sh
@@ -1,2 +1,6 @@
-cp web_toon_user_dictionary/* mecab-ko-dic-2.0.1-20150920/user-dic/
-bash mecab-ko-dic-2.0.1-20150920/tools/add-userdic.sh
+cp web_toon_user_dictionary/* mecab-ko-dic-2.0.1-20150920/
+#bash mecab-ko-dic-2.0.1-20150920/tools/add-userdic.sh
+cd mecab-ko-dic-2.0.1-20150920
+sudo make clean
+sudo make
+cd ..

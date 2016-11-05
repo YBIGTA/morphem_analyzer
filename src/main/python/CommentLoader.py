578ee03027115b5b94026838c09029064f9c3ae1
jack.yun <jack.yun@yelloexchange.com>
2016-11-06 00:25:26 +0900

    feat : modify query to use column name of db and add test case

--- src/main/python/CommentLoader.py
+++ src/main/python/CommentLoader.py
@@ -4,32 +4,31 @@ from .util import CommentDbConnector
 
 
 class CommentLoader:
-    _get_comment_count_query = "select count(*) from tb_comment " \
-                               + " where webtoon_id = %s " \
-                               + " and episode_id= %s "
-    _get_comment_query = "select * from tb_comment " \
-                         + " where webtoon_id = %s " \
-                         + " and episode_id = %s "
+    _get_comment_count_query = "select count(*) from comment " \
+                               + " where title_id = %s " \
+                               + " and episode_no= %s "
+    _get_comment_query = "select * from comment " \
+                         + " where title_id = %s " \
+                         + " and episode_no = %s "
 
     def __init__(self, host, port, password, database, user_name):
         self.comment_db_connector = CommentDbConnector(host, port, user_name, password, database)
 
-
     def load_comments(self, webtoon_id, episode_id):
         logging.info("webtoon_id : {webtoon_id}, episode_id : {episode_id} ".format(
-            {webtoon_id: webtoon_id, episode_id: episode_id}))
+            webtoon_id=webtoon_id, episode_id=episode_id))
 
         comment_size = self.get_comments_size(webtoon_id, episode_id)
 
         logging.info("webtoon_id : {webtoon_id}, episode_id : {episode_id}, comment size : {comment_size}".format(
-            {webtoon_id: webtoon_id, episode_id: episode_id, comment_size: comment_size}))
+            webtoon_id=webtoon_id, episode_id=episode_id, comment_size=comment_size[0]))
 
         cur = self.comment_db_connector.get_cursor()
         cur.execute(self._get_comment_query % (webtoon_id, episode_id))
         return cur.fetchall()
 
     def get_comments_size(self, webtoon_id, episode_id):
-        cur = self.get_cursor()
+        cur = self.comment_db_connector.get_cursor()
         cur.execute(self._get_comment_count_query % (webtoon_id, episode_id))
         webtoon_comment_count = cur.fetchone()
         return webtoon_comment_count
--- src/test/python/DbConnectTest.py
+++ src/test/python/DbConnectTest.py
@@ -1,11 +1,13 @@
 from src.main.python.util import CommentDbConnector
-
+from src.main.python.CommentLoader import CommentLoader
 import yaml
 import unittest
-
+import logging
 
 class TestDbConnection(unittest.TestCase):
     def setUp(self):
+        logging.basicConfig(level=logging.INFO)
+
         self.configuration_path = "../resource/application.yml"
         with open(self.configuration_path, 'r') as fi:
             self.conf = self._load_configuration(fi)
@@ -24,6 +26,16 @@ class TestDbConnection(unittest.TestCase):
         cur.execute("select 1")
 
         self.assertIs(cur.fetchone()[0], 1)
+
+    def test_db_comment_load(self):
+        comment_loader = CommentLoader(host=self.conf['host'],
+                                       port=self.conf['port'],
+                                       user_name=self.conf['user_name'],
+                                       password=self.conf['password'],
+                                       database=self.conf['database'])
+
+        comments = comment_loader.load_comments(self.conf['webtoon_id'], self.conf['episode_id'])
+        self.assertIsNotNone(comments)
 
 
 if __name__ == '__main__':
